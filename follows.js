bf09df66df7ccf1f9887dc0aa13a0e79dd4b6d31
phinal24 <phinal24@users.noreply.github.com>
2014-02-19 06:55:36 -0600

    Update follows.js
    personalized the presets

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'JpQgSAZ9aLybiAWUsdiMJMRDZszZGeNt5gGrI2Su8Zp8rHJt', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1085848370], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Fermentation Temperature', // Replaces Xively logo in the header
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 15, // Default interval for data to be displayed (in seconds)
+		dataColor		= '234567', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
